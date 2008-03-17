7a636a720906a5404f14e69cbec11c66312cf3ed
jashank.jeremy <jashank.jeremy@e1e7e989-aa47-0410-b903-654cdb7b429f>
2008-03-17 08:35:08 +0000

    Creating a test C file.  If this compiles, we (in theory) are successful!

--- trunk/test.c
+++ trunk/test.c
@@ -1,1 +1,17 @@
-hi
+/* XXX: This is a short piece of test code - it merely __asm("nop")'s
+ * and returns. We really should make this proper test code.
+
+ * $Id$
+ */
+
+#ifndef lint
+static char vcid[] = "$Id$";
+#endif /* lint */
+
+/* Prototypes */
+int main();
+
+/* main() code*/
+int main() {
+  return (0);
+}
